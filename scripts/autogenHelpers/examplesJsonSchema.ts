3bc1037c04cccdd8bd246e8bb4228dada2fde18b
Antony Vitré <avitre@leni.fr>
2025-11-20 19:24:37 +0100

    Fix ts check on build

--- scripts/autogenHelpers/examplesJsonSchema.ts
+++ scripts/autogenHelpers/examplesJsonSchema.ts
@@ -1,14 +1,9 @@
-import { type ExampleMetadata } from '../../example/src/examples/common/ExampleMetadata';
-
-export type { ExampleMetadata } from '../../example/src/examples/common/ExampleMetadata';
-
 export type ExampleGroupMetadata = {
   title: string;
 };
 
 export type Example = {
   name: string;
-  metadata: ExampleMetadata;
   fullPath: string;
   relPath: string;
 };
